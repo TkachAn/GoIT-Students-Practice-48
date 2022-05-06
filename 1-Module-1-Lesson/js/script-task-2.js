1767843ef695ff428632c6d33c016778399f9a00
TkachAn <4ndrey.tkachenko@gmail.com>
2022-05-06 16:51:53 +0300

    o

--- 1-Module-1-Lesson/js/script-task-2.js
+++ 1-Module-1-Lesson/js/script-task-2.js
@@ -1,5 +1,14 @@
 // Комбіновані оператори
-let students = 50;
-console.log('++', ++students);
+// let students = 50;
+// console.log('++', ++students);
 
-console.log('total', students);
+// console.log('total', students);
+const fruits = ["apple", "plum", "pear", "orange"];
+
+// Change code below this line
+const firstElement = firsts[0];
+const secondElement = firsts[1];
+const lastElement = firsts[length-1];
+console.log('firstElement', firstElement);
+console.log('secondElement', secondElement);
+console.log('lastElement', lastElement);
--- 1-Module-2-Lesson/js/script-task-10.js
+++ 1-Module-2-Lesson/js/script-task-10.js
@@ -1,1 +1,40 @@
-// check password
+const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+  
+  order(pizzaName, onSuccess, onError) {
+    // for(let i = 0; i < this.pizzas.length; i++){
+    //   if(this.pizzas[i] === pizzaName){
+    //     console.log("onSuccess", this.pizzas[i]);
+    //      return onSuccess(pizzaName);
+    //   }
+    // }
+    numbers.forEach(function (number, index) {
+      console.log(`Индекс ${index}, значение ${number}`);
+    });
+
+    pizzas.forEach((el, el.pizza === pizzaName) {
+      return onSuccess(pizzaName);
+    };
+    console.log("error", pizzaName);
+    return onError(pizzaName);
+    )},
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! There is no pizza with a name ${error} in the assortment.`;//`Error! There is no pizza with a name ${pizzaName} in the assortment.`
+}
+
+// Method calls with callbacks
+console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+//console.log(pizzaPalace.order());
+// console
